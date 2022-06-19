f0e5da30808993053f9b1813f1d821b7e75ddb26
YangMBP <ypp_p@icloud.com>
2022-06-20 04:25:20 +0800

    feature:find target sum ways

---
+++ medium/findTargetSumWays.go
@@ -0,0 +1,77 @@
+package medium
+
+import "math"
+
+/**
+给你一个整数数组 nums 和一个整数 target 。
+
+向数组中的每个整数前添加'+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+
+例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+
+
+
+示例 1：
+
+输入：nums = [1,1,1,1,1], target = 3
+输出：5
+解释：一共有 5 种方法让最终目标和为 3 。
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+示例 2：
+
+输入：nums = [1], target = 1
+输出：1
+
+
+提示：
+
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+0 <= sum(nums[i]) <= 1000
+-1000 <= target <= 1000
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/target-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+既然为target，那么就一定有 left组合 - right组合 = target。
+
+left + right等于sum，而sum是固定的。
+
+公式来了， left - (sum - left) = target -> left = (target + sum)/2 。
+
+target是固定的，sum是固定的，left就可以求出来。
+
+此时问题就是在集合nums中找出和为left的组合。
+*/
+func findTargetSumWays(nums []int, target int) int {
+	sum := 0
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		sum += nums[i]
+	}
+	weight := (sum + target) / 2
+	//dp[j]表示和为j时有多少种组合
+	if weight%2 == 1 {
+		return 0
+	}
+	if math.Abs(float64(target)) > float64(sum) {
+		return 0
+	}
+
+	dp := make([]int, weight+1)
+	dp[0] = 1
+	for i := 0; i < l; i++ {
+		for j := weight; j >= nums[i]; j-- {
+			dp[j] = dp[j] + dp[j-nums[i]]
+		}
+	}
+	return dp[target]
+}
